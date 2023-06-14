ee77f7b9bb0a27a81e6deb85c8523979551d1081
babakthebest <babakthebest@gmail.com>
2023-06-14 23:12:39 +0330

    drop down search

--- app/user/layout.jsx
+++ app/user/layout.jsx
@@ -1,27 +1,24 @@
-'use client'
 import SidebarContent  from "@/components/sidebar/SideBarContent";
 import SidebarContainer  from '@/components/sidebar/SidebarContainer';
 import SidebarBodyContainer  from '@/components/sidebar/SidebarBodyContainer';
-import { useBarandazContext } from '@/context/context';
+// import { useBarandazContext } from '@/context/context';
 import Login from "@/components/UTILS/Login";
 
 
 export default async function DashboardLayout({ children }) {
   // const router=useRouter()
-  const { auth, setAuth } = useBarandazContext();
+  // const { auth, setAuth } = useBarandazContext();
 
   return (
     <>
-      {auth == "authenticated" ? (
+      
         <SidebarContent>
           <SidebarContainer title="سایدبار" mobile={false}>
             {/* <Categories data={allCategories}> </Categories> */}
           </SidebarContainer>
           <SidebarBodyContainer>{children}</SidebarBodyContainer>
         </SidebarContent>
-      ) : (
-        <Login />
-      )}
+     
     </>
   );
 }
